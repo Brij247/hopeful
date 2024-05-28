27f64879cc01a7ce966ac2b1419d9afba2b9f458
Brijitha AV <brijithaav@gmail.com>
2024-05-28 23:00:28 +0530

    feat(homepage): Add header

--- src/layout/ui/Header.jsx
+++ src/layout/ui/Header.jsx
@@ -1,5 +1,18 @@
+import NavBar from "./NavBar";
+import NavItem from "./NavItem";
+
 function Header() {
-  return <div>EXCEL HOPE</div>;
+  return (
+    <div className="divide-y divide-slate-100">
+      <NavBar>
+        <NavItem href="/home" isActive>
+          Home
+        </NavItem>
+        <NavItem href="/about">About</NavItem>
+        <NavItem href="/projects">Projects</NavItem>
+      </NavBar>
+    </div>
+  );
 }
 
 export default Header;
